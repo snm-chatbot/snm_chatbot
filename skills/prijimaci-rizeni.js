912d3f98d2fa8db6419f3eef50260e92d0fe2bc5
Zuzana Dostalova <dostalovaz@gmail.com>
2017-04-06 00:36:41 +0200

    Prijimaci rizeni druha cast

--- skills/prijimaci-rizeni.js
+++ skills/prijimaci-rizeni.js
@@ -33,8 +33,36 @@ module.exports = function(controller) {
       bot.reply(message, gif);
   });
 
-  controller.hears(['uk[a]ázkov[yý] test', 'zkusit', 'nane[cč]isto'], 'message_received,facebook_postback', function(bot, message) {
-      bot.reply(message, 'Zkusit si přijímačku můžeš tady: http://snmprijimacky.cz/testy/');
+  controller.hears(['kolik stoj[ií] .* p[rř]ihl[aá][sš]ka', 'za kolik je p[rř]ihl[aá][sš]ka', 'cena .* p[rř]ihl[aá]šk[ya]'], 'message_received,facebook_postback', function(bot, message) {
+      var question = function(err, convo) {
+          convo.ask('500Kč. A víš co ještě stojí 500Kč?', function(response, convo) {
+              convo.say('17 piv. To je ta přihláška lepší, ne?');
+              convo.next();
+          });
+      };
+
+      bot.startConversation(message, question);
+  });
+
+  controller.hears(['[uú]stn[íi]'], 'message_received,facebook_postback', function(bot, message) {
+      bot.reply(message, 'Ústní kolo vypadá asi méně eroticky, než si myslíš. Musíš si připravit literaturu, diplomku, a tak dále.');
+  });
+
+  controller.hears(['jak .* p[řr]ihl[aá]sit', 'podat p[řr]ihl[áa][šs]ku'], 'message_received,facebook_postback', function(bot, message) {
+      bot.reply(message, 'Příhlášku muže podat tady http://is.cuni.cz/studium/prijimacky/index.php?do=detail_obor&id_obor=17437');
+  });
+
+  controller.hears(['kolik .* hl[áa]s[íi]', 'kolik .* p[rř]ij[íi]m[áa]', 'kolik berou', 'kolik .* bere'], 'message_received,facebook_postback', function(bot, message) {
+      bot.reply(message, 'Hlásí se tak 200 lidí, a konkurence stoupá!');
+      bot.reply(message, 'No a bere se cca 25...');
+  });
+
+  controller.hears(['kde .* p[řr]ij[ií]ma[čc]ky'], 'message_received,facebook_postback', function(bot, message) {
+      bot.reply(message, 'Písmená část je na fildě ve městě a ústní na konci světa v Jinonicích');
+  });
+
+  controller.hears(['kolik .* komis[ei]', 'komis[ei] .* kolik'], 'message_received,facebook_postback', function(bot, message) {
+      bot.reply(message, 'Komise na ústní část se skládá alespoň ze 3 expertů na problematiku.');
   });
 }
 
